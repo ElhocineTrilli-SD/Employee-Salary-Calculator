bd2c0d05fd405e4dfb5ff644555df91c7b097f7b
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-07-06 14:41:23 +0100

    add dataaccess layer and created add employee method

---
+++ DataAccess/clsConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    internal class clsConnection
+    {
+        public static string     ConString = "Server=.;Database = EmployeeMS;User Id=sa;Password=NewStrongPassword123!";
+        
+       
+
+    }
+}
---
+++ DataAccess/clsEmployeeData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataAccess
+{
+    public class clsEmployeeData
+    {
+        public static bool  AddNewEmployee(string Name, string Gender, string Phone, string Position, int Salary, string JDate)
+        {
+            int RowAffected = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsConnection.ConString))
+                {
+                    connection.Open();
+                    string Query = "insert into Employees values('{0}','{1}','{2}','{3}','{4}','{5}')";
+                    Query = string.Format(Query, Name, Gender, Phone, Position, Salary, JDate);
+
+                    using (SqlCommand command = new SqlCommand(Query, connection))
+                    {
+                      RowAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex) { }
+                return RowAffected > 0;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
