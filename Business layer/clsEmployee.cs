0fdf4601180966b194347354c411664d3b13fe49
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-07-06 14:41:54 +0100

    add business layer

---
+++ Business layer/clsEmployee.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess;
+
+namespace Business_layer
+{
+    public static  class clsEmployee
+    {
+
+        public static bool AddNewEmployee(string Name, string Gender, string Phone, string Position, int Salary, string JDate)
+        {
+            return clsEmployeeData.AddNewEmployee(Name, Gender, Phone, Position, Salary, JDate);
+
+        }
+
+
+    }
+}
