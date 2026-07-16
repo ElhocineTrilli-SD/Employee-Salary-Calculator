c0c272c2b0018cf269263b15fa12a852da22d3e0
HusseinTrilli-DEV <husseintrilli@gmail.com>
2026-07-17 00:03:05 +0100

    update

--- Project Employee Salary Calculator/Employees/frmEmployee.cs
+++ Project Employee Salary Calculator/Employees/frmEmployee.cs
@@ -216,12 +216,7 @@ namespace Employee_Salary_Calculator.Dashbord
         }
         private void btnAnalytics_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(
-    "This feature is not available in this version yet.",
-    "Coming Soon",
-    MessageBoxButtons.OK,
-    MessageBoxIcon.Information
-);
+          
         }
     }
 }
